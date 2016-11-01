8a92642e75e46407b109872f35363e445e0ccf8d
Trần Quốc Thông <tqthong0911@gmail.com>
2016-11-01 16:52:22 +0700

    finish login

--- src/app/modules/login/controllers/login.js
+++ src/app/modules/login/controllers/login.js
@@ -1,11 +1,24 @@
-define(function(require){
+define(function(require) {
     'use strict';
 
     var angular = require('angular');
 
-    ctrlFn.$inject = ['$scope'];
-    function ctrlFn($scope){
-        //Nội dung của controller ghi ở đây
+
+    ctrlFn.$inject = ['$scope', '$window', 'UserService', '$state'];
+
+    function ctrlFn($scope, $window, UserService, $state) {
+        $scope.email = '';
+        $scope.password = '';
+        $scope.login = function() {
+                if ($scope.email != '' && $scope.password.length >= 6) {
+                    UserService.login($scope.email, $scope.password).then(function(success) {
+                        $state.go('base3.booking'); //test di
+                    }, function(err) {
+                        console.log(err);
+                    });
+                }
+            }
+            //Nội dung của controller ghi ở đây
         console.log('đang ở login');
     }
 
